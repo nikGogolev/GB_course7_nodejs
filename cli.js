e6b34eece5685991aab26cacd839b636fff99e3f
nikson1701 <78842306+nikson1701@users.noreply.github.com>
2021-11-19 20:33:56 +0300

    lesson4Hw
    Осуществляет просмотр каталога, может переходить в другие каталоги и просматривать файлы
    По ключу -p передается путь к директории для просмотра, по ключу -s строка для поиска

--- cli.js
+++ cli.js
@@ -20,6 +20,26 @@ const executionDir = options.path || process.cwd();
 
 const isFile = (dir, fileName) => fs.lstatSync(path.resolve(dir, fileName)).isFile();
 
+if (options.path && isFile(options.path, '')) {
+    const data = fs.readFileSync(path.resolve(options.path, ''), 'utf-8');
+    if (options.search) {
+        let pos = 0;
+        let findCount = 0;
+        while (true) {
+            let foundPos = data.indexOf(options.search, pos);
+            if (foundPos == -1) break;
+            pos = foundPos + 1;
+            findCount++;
+        }
+        console.log(data.replaceAll(options.search, '\033[46m' + options.search + '\033[0m'));
+        console.log('\033[46m Найдено ' + findCount + ' совпадений \033[0m');
+    } else {
+        console.log(data);
+    }
+    return;
+}
+
+
 const showDirContent = (dir) => {
     const list = fs.readdirSync(path.resolve(dir));
     inquirer.prompt([
@@ -30,10 +50,10 @@ const showDirContent = (dir) => {
             choices: list,
         },
     ]).then(({ fileName }) => {
-        if(isFile(dir, fileName)){
+        if (isFile(dir, fileName)) {
             const fullFilePath = path.resolve(dir, fileName);
             const data = fs.readFileSync(fullFilePath, 'utf-8');
-            if(options.search){
+            if (options.search) {
                 let pos = 0;
                 let findCount = 0;
                 while (true) {
